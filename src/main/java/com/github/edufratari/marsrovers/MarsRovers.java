700d262690ce1763c7e7067427fff169331dc522
Eduardo Dib Fratari <edufratari@hotmail.com>
2017-05-09 17:23:25 -0300

    Atualização

--- src/main/java/com/github/edufratari/marsrovers/MarsRovers.java
+++ src/main/java/com/github/edufratari/marsrovers/MarsRovers.java
@@ -11,32 +11,32 @@ public class MarsRovers {
 		RoboticRovers roboticRovers1 = new RoboticRovers(0, 0, "N", plateau);
 		RoboticRovers roboticRovers2 = new RoboticRovers(0, 0, "N", plateau);
 
-		Scanner s = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Enter the value of the  xPosition, yPosition and direction of rover 1: ");
-		String line = s.nextLine();
+		String line = scanner.nextLine();
 		String[] parts = line.split(" ");
 		int x = Integer.parseInt(parts[0]);
 		int y = Integer.parseInt(parts[1]);
 		String n = parts[2];
 		System.out.println("Enter the command for the rover 1: ");
-		String c = s.next();
+		String c = scanner.nextLine();
 		
 		roboticRovers1.setPosition(x, y, n);
 		roboticRovers1.command(c);
 		roboticRovers1.printPosition();
 
 		
-		Scanner s1 = new Scanner(System.in);
+		//Scanner s1 = new Scanner(System.in);
 
 		System.out.println("Enter the value of the  xPosition, yPosition and direction of rover 2: ");
-		String line1 = s1.nextLine();
+		String line1 = scanner.nextLine();
 		String[] parts1 = line1.split(" ");
 		int x1 = Integer.parseInt(parts1[0]);
 		int y1 = Integer.parseInt(parts1[1]);
 		String n1 = parts1[2];
 		System.out.println("Enter the command for the rover 2: ");
-		String c1 = s1.next();
+		String c1 = scanner.next();
 		
 		roboticRovers2.setPosition(x1, y1, n1);
 		roboticRovers2.command(c1);
